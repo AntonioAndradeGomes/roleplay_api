5f73ba19e48c811d3677bdf8309ceca7162fe4f3
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-02-06 17:39:24 -0300

    Testing the user registration with invalid data

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -12,55 +12,71 @@ test.group("user", (group) => {
       email: "teste@teste.com",
       username: "teste",
       password: "teste",
-      avatar: 'http://images.com/image/1'
+      avatar: "http://images.com/image/1",
     };
     //se a rota não existe (404) deve ser implementada no projeto
-    const {body} = await supertest(BASEURL).post("/users").send(userPayload).expect(201);
+    const { body } = await supertest(BASEURL)
+      .post("/users")
+      .send(userPayload)
+      .expect(201);
 
     //validar o corpo retornado
-    assert.exists(body.user, 'User undefined');
-    assert.exists(body.user.id, 'Id undefined');
+    assert.exists(body.user, "User undefined");
+    assert.exists(body.user.id, "Id undefined");
     assert.equal(body.user.email, userPayload.email);
     assert.equal(body.user.username, userPayload.username);
-    assert.notExists(body.user.password, 'Password defined');
+    assert.notExists(body.user.password, "Password defined");
     assert.equal(body.user.avatar, userPayload.avatar);
   });
 
-  test('it should return 409 when email is already in use', async (assert) => {
+  test("it should return 409 when email is already in use", async (assert) => {
     const { email } = await UserFactory.create();
-    const {body} = await supertest(BASEURL).post("/users").send({
-      username: 'teste',
-      email,
-      password: "teste",
-      avatar: 'http://images.com/image/1'
-    }).expect(409);
+    const { body } = await supertest(BASEURL)
+      .post("/users")
+      .send({
+        username: "teste",
+        email,
+        password: "teste",
+        avatar: "http://images.com/image/1",
+      })
+      .expect(409);
 
     assert.exists(body.message);
-    assert.include(body.message, 'email');
-    assert.equal(body.code, 'BAD_REQUEST');
+    assert.include(body.message, "email");
+    assert.equal(body.code, "BAD_REQUEST");
     assert.equal(body.status, 409);
   });
 
-  test.only('it should return 409 when username is already in use', async (assert) => {
+  test("it should return 409 when username is already in use", async (assert) => {
     const { username } = await UserFactory.create();
-    const {body} = await supertest(BASEURL).post("/users").send({
-      username,
-      email: 'teste@teste.com',
-      password: "teste",
-      avatar: 'http://images.com/image/1'
-    }).expect(409);
+    const { body } = await supertest(BASEURL)
+      .post("/users")
+      .send({
+        username,
+        email: "teste@teste.com",
+        password: "teste",
+        avatar: "http://images.com/image/1",
+      })
+      .expect(409);
 
     assert.exists(body.message);
-    assert.include(body.message, 'username');
-    assert.equal(body.code, 'BAD_REQUEST');
+    assert.include(body.message, "username");
+    assert.equal(body.code, "BAD_REQUEST");
     assert.equal(body.status, 409);
+  });
+
+  test.only("it should return 422 when required data is not provided", async (assert) => {
+
+    const {body} = await supertest(BASEURL).post('/users').send({}).expect(422);
+    assert.equal(body.code,'BAD_REQUEST');
+    assert.equal(body.status, 422);
   });
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction();
   });
 
-  group.afterEach(async  () => {
+  group.afterEach(async () => {
     await Database.rollbackGlobalTransaction();
-  })
+  });
 });
