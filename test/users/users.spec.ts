6928b514e401a2757909fe877e424dfd0db93d9a
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-02-06 15:18:48 -0300

    test user router return 201

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,7 +1,17 @@
-import test from 'japa';
+import test from "japa";
+import supertest from "supertest";
 
-test.group('user', ()=> {
-  test('it should create an user', (assert) => {
-    assert.isTrue(true);
+const BASEURL = `http://${process.env.HOST}:${process.env.PORT}`;
+
+test.group("user", () => {
+  test.only("it should create an user", async (assert) => {
+    //payload do user
+    const userPayload = {
+      email: "teste@teste.com",
+      username: "teste",
+      password: "teste",
+    };
+    //se a rota não existe (404) deve ser implementada no projeto
+    await supertest(BASEURL).post("/users").send(userPayload).expect(201);
   });
 });
