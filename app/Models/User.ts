2da1f66ccdf749bba35e7fd66ed77bde32fe040d
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-02-06 15:51:49 -0300

    Introduction to Model Hooks: encrypting sensitive user data

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,25 +1,31 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, beforeSave, column } from "@ioc:Adonis/Lucid/Orm";
+import Hash from "@ioc:Adonis/Core/Hash";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number;
 
   @column()
-  public username: String;
+  public username: string;
 
   @column()
-  public email: String;
+  public email: string;
+
+  @column({ serializeAs: null })
+  public password: string;
 
   @column()
-  public password: String;
-
-  @column()
-  public avatar: String;
+  public avatar: string;
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime;
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) user.password = await Hash.make(user.password);
+  }
 }
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -13,14 +13,14 @@ test.group("user", () => {
       avatar: 'http://images.com/image/1'
     };
     //se a rota não existe (404) deve ser implementada no projeto
-     const {body} = await supertest(BASEURL).post("/users").send(userPayload).expect(201);
+    const {body} = await supertest(BASEURL).post("/users").send(userPayload).expect(201);
 
     //validar o corpo retornado
     assert.exists(body.user, 'User undefined');
     assert.exists(body.user.id, 'Id undefined');
     assert.equal(body.user.email, userPayload.email);
     assert.equal(body.user.username, userPayload.username);
-    assert.equal(body.user.password, userPayload.password);
+    assert.notExists(body.user.password, 'Password defined');
     assert.equal(body.user.avatar, userPayload.avatar);
   });
 });
