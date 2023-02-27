b2c0254ab8735dad72c438d248cbefb1732a3203
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-02-27 16:52:13 -0300

    Testing login error scenarios

--- test/sessions/sessions.spec.ts
+++ test/sessions/sessions.spec.ts
@@ -12,7 +12,7 @@ test.group("Session", (group) => {
       password: plainPassword,
     }).create();
 
-    const {body} = await supertest(BASEURL)
+    const { body } = await supertest(BASEURL)
       .post("/sessions")
       .send({
         email,
@@ -20,7 +20,7 @@ test.group("Session", (group) => {
       })
       .expect(201);
     console.log(body.user);
-    assert.isDefined(body.user, 'User undefined');
+    assert.isDefined(body.user, "User undefined");
     assert.equal(body.user.id, id);
   });
 
@@ -34,13 +34,13 @@ test.group("Session", (group) => {
       }
     }
   */
-  test.only('it should return an api token when session in created', async (assert) => {
+  test("it should return an api token when session in created", async (assert) => {
     const plainPassword = "test";
     const { id, email } = await UserFactory.merge({
       password: plainPassword,
     }).create();
 
-    const {body} = await supertest(BASEURL)
+    const { body } = await supertest(BASEURL)
       .post("/sessions")
       .send({
         email,
@@ -48,10 +48,30 @@ test.group("Session", (group) => {
       })
       .expect(201);
 
-    assert.isDefined(body.token, 'Token undefined');
+    assert.isDefined(body.token, "Token undefined");
     assert.equal(body.user.id, id);
   });
 
+  test("it should return 400 when credentials are not provided", async (assert) => {
+    const { body } = await supertest(BASEURL)
+      .post("/sessions")
+      .send({})
+      .expect(400);
+
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 400);
+  });
+
+  test.only("it should return 400 when credentials are invalid", async (assert) => {
+    const { email } = await UserFactory.create();
+    const { body } = await supertest(BASEURL)
+      .post("/sessions")
+      .send({ email, password: "test" })
+      .expect(400);
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 400);
+    assert.equal(body.message, 'invalid credentials');
+  });
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction();
