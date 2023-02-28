9dad8994d64cac3c52205f48da7d1937aecf3b42
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-02-28 15:00:21 -0300

    Testing the table creation

--- start/routes.ts
+++ start/routes.ts
@@ -4,9 +4,17 @@ Route.get("/", async () => {
   return { message: "hello world" };
 });
 
+//users
 Route.post("/users", "UsersController.store");
 Route.put("/users/:id", "UsersController.update").middleware("auth");
+
+//password
 Route.post("/forgot-password", "PasswordsController.forgotPassword");
 Route.post("/reset-password", "PasswordsController.resetPassword");
+
+//auth, sessions, login
 Route.post("/sessions", "SessionsController.store");
 Route.delete("/sessions", "SessionsController.destroy");
+
+//tables -> messas
+Route.post("/groups", 'GroupsController.store');
